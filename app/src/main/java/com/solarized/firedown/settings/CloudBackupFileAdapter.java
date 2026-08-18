6e039cd730a4bdedc42081cc7f5d06baf1b89b1b
solarizeddev <info@solarized.dev>
2026-08-19 01:08:27 +0200

    - Bump to 1191 - Update to gecko 154.0.20260814215756

--- app/src/main/java/com/solarized/firedown/settings/CloudBackupFileAdapter.java
+++ app/src/main/java/com/solarized/firedown/settings/CloudBackupFileAdapter.java
@@ -38,7 +38,6 @@ import com.solarized.firedown.utils.SelectionStyling;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
